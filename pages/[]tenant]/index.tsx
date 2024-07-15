76c04243f47eaf59eb815366e768f5e1b12d5af1
Morgan <morgan.espindola@gmail.com>
2024-07-15 07:22:43 -0300

    Basic Structure

---
+++ pages/[]tenant]/index.tsx
@@ -0,0 +1,9 @@
+const Tenant = () => {
+  return (
+    <div className="">
+      <h1>Tenant</h1>
+    </div>
+  );
+};
+
+export default Tenant;
---
+++ pages/[]tenant]/orders.tsx
@@ -0,0 +1,9 @@
+const Orders = () => {
+  return (
+    <div className="">
+      <h1 className="text-3xl">Orders</h1>
+    </div>
+  );
+};
+
+export default Orders;
