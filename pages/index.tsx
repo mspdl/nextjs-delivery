1f7c6f83b6b68fc9d50d5446273d33b9d75f95bd
Morgan <morgan.espindola@gmail.com>
2024-07-15 07:55:43 -0300

    Menu screen: header - part 1

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,18 @@
 const Home = () => {
   return (
-    <div className="">
-      <h1 className="text-3xl">Home</h1>
+    <div className="bg-white">
+      <header className="bg-[#F9F9FB] pt-14 pr-6 pb-8 pl-6">
+        <div className="">
+          <div className="">
+            <h1 className="font-semibold text-2xl text-[#1b1b1b]">
+              Welcome 👋
+            </h1>
+            <p className="font-normal text-base text-[#979797cc]">How can we help you?</p>
+          </div>
+          <div className=""></div>
+        </div>
+        <div className="">Search</div>
+      </header>
     </div>
   );
 };
