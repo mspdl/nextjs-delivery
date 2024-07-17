e244aa38d0928fe0fe4bbdbe39e8bd11eca9ce52
Morgan <morgan.espindola@gmail.com>
2024-07-16 23:36:28 -0300

    Menu screen: header: Hamburguer menu

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,14 +2,22 @@ const Home = () => {
   return (
     <div className="bg-white">
       <header className="bg-[#F9F9FB] pt-14 pr-6 pb-8 pl-6">
-        <div className="">
+        <div className="flex justify-between items-center">
           <div className="">
-            <h1 className="font-semibold text-2xl text-[#1b1b1b]">
+            <h1 className="font-semibold text-2xl text-[#1b1b1b] mb-2">
               Welcome 👋
             </h1>
-            <p className="font-normal text-base text-[#979797cc]">How can we help you?</p>
+            <p className="font-normal text-base text-[#979797cc]">
+              How can we help you?
+            </p>
           </div>
-          <div className=""></div>
+          <div className="">
+            <div className="w-6 h-5 flex flex-col justify-between">
+              <div className="bg-[#fb9400] h-1 rounded-md"></div>
+              <div className="bg-[#fb9400] h-1 rounded-md"></div>
+              <div className="bg-[#fb9400] h-1 rounded-md"></div>
+            </div>
+          </div>
         </div>
         <div className="">Search</div>
       </header>
