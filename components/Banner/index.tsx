36006907304d29ff1584fcf60b240866db0dbfdb
Morgan <morgan.espindola@gmail.com>
2024-07-24 07:26:21 -0300

    Banner - part 3

--- components/Banner/index.tsx
+++ components/Banner/index.tsx
@@ -1,11 +1,17 @@
 import Image from "next/image";
 import "swiper/css";
+import { Autoplay } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 export const Banner = () => {
   return (
     <div className="m-6">
-      <Swiper slidesPerView={1}>
+      <Swiper
+        slidesPerView={1}
+        autoplay={{ delay: 2500, disableOnInteraction: false }}
+        loop={true}
+        modules={[Autoplay]}
+      >
         <SwiperSlide className="text-center text-lg bg-white !flex justify-center items-center">
           <Image
             className="block w-max object-cover"
