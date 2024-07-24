be22c5bfb8f778ee097f6ae0f9bf78562d95eaec
Morgan <morgan.espindola@gmail.com>
2024-07-24 07:19:17 -0300

    Banner - part 2

--- components/Banner/index.tsx
+++ components/Banner/index.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import "swiper/css";
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -5,10 +6,24 @@ export const Banner = () => {
   return (
     <div className="m-6">
       <Swiper slidesPerView={1}>
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide className="text-center text-lg bg-white !flex justify-center items-center">
+          <Image
+            className="block w-max object-cover"
+            src="/tmp/banner1.png"
+            alt=""
+            width={700}
+            height={100}
+          />
+        </SwiperSlide>
+        <SwiperSlide className="text-center text-lg bg-white !flex justify-center items-center">
+          <Image
+            className="block w-max object-cover"
+            src="/tmp/banner2.png"
+            alt=""
+            width={700}
+            height={100}
+          />
+        </SwiperSlide>
       </Swiper>
     </div>
   );
