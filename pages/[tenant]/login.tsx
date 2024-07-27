fc45b9e04099f5b4726a05099ecd3490a8b8e9cd
Morgan <morgan.espindola@gmail.com>
2024-07-27 07:47:10 -0300

    Creating Login Page

---
+++ pages/[tenant]/login.tsx
@@ -0,0 +1,43 @@
+import { useAppContext } from "@/contexts/AppContext";
+import { useApi } from "@/libs/useApi";
+import { Tenant } from "@/types/Tenant";
+import { GetServerSideProps } from "next";
+import Head from "next/head";
+import { useEffect } from "react";
+
+const Login = (data: Props) => {
+  const { tenant, setTenant } = useAppContext();
+
+  useEffect(() => {
+    setTenant(data.tenant);
+  }, []);
+
+  return (
+    <div className="">
+      <Head>
+        <title>Login | {data.tenant.name}</title>
+      </Head>
+      <div>Login</div>
+    </div>
+  );
+};
+
+export default Login;
+
+type Props = {
+  tenant: Tenant;
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { tenant: tenantSlug } = context.query;
+
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const api = useApi();
+  const tenant = await api.getTenant(tenantSlug as string);
+
+  if (!tenant) {
+    return { redirect: { destination: "/", permanent: false } };
+  }
+
+  return { props: { tenant } };
+};
